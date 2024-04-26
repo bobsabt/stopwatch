3426504c71dccbb4c04155368ff50075d062aba3
bobsabt <bodai.barbara@gmail.com>
2024-04-26 11:05:39 +0200

    add moible view

--- stopwatch/src/components/Stopwatch.jsx
+++ stopwatch/src/components/Stopwatch.jsx
@@ -32,17 +32,17 @@ function Stopwatch(){
         setIsRunning(false);
     }
     function formatTime(){
-        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        //let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
         let minutes = Math.floor(elapsedTime / (1000 * 60)  % 60);
         let seconds = Math.floor(elapsedTime / (1000) % 60);
         let milliseconds = Math.floor(elapsedTime % 1000 / 10);
 
-        hours = String(hours).padStart(2, "0");
+        //hours = String(hours).padStart(2, "0");
         minutes = String(minutes).padStart(2, "0");
         seconds = String(seconds).padStart(2, "0");
         milliseconds = String(milliseconds).padStart(2, "0");
 
-        return `${hours}:${minutes}:${seconds}:${milliseconds} `;
+        return `${minutes}:${seconds}:${milliseconds} `;
     }
     return(
         <div className="stopwatch">
